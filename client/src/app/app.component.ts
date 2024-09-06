803817396a02a356f8c2c374b24280dbf672ddef
kwinkich <vankord21@gmail.com>
2024-09-07 00:24:06 +0300

    added some shit code and logic

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CreateHComponent } from './components/create-h/create-h.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CreateHComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'client';
---
+++ client/src/app/components/create-h/create-h.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-h',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-h.component.html',
+  styleUrl: './create-h.component.scss',
+})
+export class CreateHComponent {
+  public bgColor: string = '#000000';
+  public textColor: string = '#000000';
+  public hName: string = 'Name';
+  public hDate: string = 'Date';
+  public hTime: string = 'Time';
+  public fTotalTime: any;
+
+  public days: number = 0;
+  public hours: number = 0;
+  public minutes: number = 0;
+  public seconds: number = 0;
+
+  public getValue(event: Event): string {
+    return (event.target as HTMLInputElement).value;
+  }
+
+  public changeBgColor(event: Event) {
+    this.bgColor = (event.target as HTMLInputElement).value;
+    document.body.style.backgroundColor = this.bgColor;
+  }
+
+  public changeTextColor(event: Event) {
+    this.textColor = (event.target as HTMLInputElement).value;
+    document.body.style.color = this.textColor;
+  }
+
+  public formateTotalTime() {
+    this.fTotalTime = new Date(`${this.hDate} ${this.hTime}`);
+    return this.fTotalTime;
+  }
+}
